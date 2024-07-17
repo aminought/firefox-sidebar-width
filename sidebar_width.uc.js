6777f2025522d5ec55cc2ac263b6b20e6a925439
aminought <aminought@gmail.com>
2024-07-17 13:29:07 +0300

    little optimization

--- sidebar_width.uc.js
+++ sidebar_width.uc.js
@@ -1,67 +1,58 @@
 (function sidebar_width() {
-    'use strict';
+  "use strict";
 
-    const TIMEOUT = 1000;
-    const PREF = 'sidebar.widths';
+  const TIMEOUT = 1000;
+  const PREF = "sidebar.widths";
 
-    class SidebarWidth {
-        constructor() {
-            this.sidebarWidths = this.initWidth();
-            this.observer = this.createSidebarBoxObserver();
-        }
-
-        initWidth() {
-            try {
-                return JSON.parse(Services.prefs.getStringPref(PREF));
-            } catch (error) {
-                return Object.fromEntries(
-                    Array.from(SidebarController.sidebars.keys()).map((key) => [
-                        key,
-                        this.getCurrentWidth(),
-                    ])
-                );
-            }
-        }
-
-        createSidebarBoxObserver() {
-            const observer = new MutationObserver((records) => {
-                for (const record of records) {
-                    if (record.type === 'attributes' && record.attributeName === 'style') {
-                        this.saveWidth();
-                        break;
-                    }
-                    if (record.type === 'attributes' && record.attributeName === 'sidebarcommand') {
-                        this.restoreWidth();
-                        break;
-                    }
-                }
-            });
-            observer.observe(SidebarController._box, { attributes: true });
-            return observer;
-        }
-
-        saveWidth() {
-            this.sidebarWidths[this.getCurrentCommand()] = this.getCurrentWidth();
-            Services.prefs.setStringPref(PREF, JSON.stringify(this.sidebarWidths));
-        }
-
-        restoreWidth() {
-            SidebarController._box.style.width = this.sidebarWidths[this.getCurrentCommand()];
-        }
-
-        getCurrentWidth() {
-            return SidebarController._box.style.width;
-        }
-
-        getCurrentCommand() {
-            return SidebarController._box.getAttribute('sidebarcommand');
-        }
+  class SidebarWidth {
+    constructor() {
+      this.sidebarWidths = this.initWidth();
+      this.addEventListeners();
     }
 
-    var interval = setInterval(() => {
-        if (document.querySelector('#browser')) {
-            window.sidebarWidth = new SidebarWidth();
-            clearInterval(interval);
-        }
-    }, TIMEOUT);
+    initWidth() {
+      try {
+        return JSON.parse(Services.prefs.getStringPref(PREF));
+      } catch (error) {
+        const sidebars = Array.from(SidebarController.sidebars.keys());
+        return Object.fromEntries(
+          sidebars.map((sidebar) => [sidebar, this.width])
+        );
+      }
+    }
+
+    addEventListeners() {
+      SidebarController._box.addEventListener("resize", () => this.saveWidth());
+      addEventListener("SidebarShown", () => this.restoreWidth());
+    }
+
+    saveWidth() {
+      if (
+        this.command in this.sidebarWidths &&
+        this.sidebarWidths[this.command] === this.width
+      )
+        return;
+      this.sidebarWidths[this.command] = this.width;
+      Services.prefs.setStringPref(PREF, JSON.stringify(this.sidebarWidths));
+    }
+
+    restoreWidth() {
+      SidebarController._box.style.width = this.sidebarWidths[this.command];
+    }
+
+    get width() {
+      return SidebarController._box.style.width;
+    }
+
+    get command() {
+      return SidebarController._box.getAttribute("sidebarcommand");
+    }
+  }
+
+  var interval = setInterval(() => {
+    if (document.querySelector("#browser")) {
+      window.sidebarWidth = new SidebarWidth();
+      clearInterval(interval);
+    }
+  }, TIMEOUT);
 })();
